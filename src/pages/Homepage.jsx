1f3dd34b0446b394e1067823bb153f7672874271
Daniel Keum <dkeum@sfu.ca>
2026-08-28 16:13:08 -0700

    test

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -41,6 +41,10 @@ const Homepage = () => {
     <div className="max-w-6xl mx-auto overflow-x-hidden">
       <Navbar />
 
+      <a href="https://smollaunchpad.com/projects/mathmagick?utm_source=badge" target="_blank" rel="noopener noreferrer" className="flex justify-center mt-6 md:mt-10">
+        <img src="https://smollaunchpad.com/smollaunchpad/images/badges/featured-on-light.svg" alt="Featured on Smol LaunchPad" style={{ height: "44px", width: "auto" }} />
+      </a>
+
       {/* ── HERO ── */}
       <div className="relative flex min-h-[44rem] md:h-[44rem] w-full justify-center bg-white dark:bg-black mt-3 overflow-hidden">
         <div
@@ -605,9 +609,7 @@ const Homepage = () => {
           </div>
         </div>
 
-        <a href="https://smollaunchpad.com/projects/mathmagick?utm_source=badge" target="_blank" rel="noopener noreferrer" className="flex justify-center mt-6 md:mt-10">
-          <img src="https://smollaunchpad.com/smollaunchpad/images/badges/featured-on-light.svg" alt="Featured on Smol LaunchPad" style={{ height: "44px", width: "auto" }} />
-        </a>
+
       </section>
 
 
